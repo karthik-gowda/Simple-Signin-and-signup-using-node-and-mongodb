1edcc53ed495eb216004e8bf520bfbdf1e7d6caf
Karthik BT <79859147+karthikdiscoveries@users.noreply.github.com>
2022-01-29 16:48:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 5000 || process.env.PORT;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://karthikbt22:karthikbt22@cluster0.lxu0r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/", require("./routes/user.route"));
+
+app.listen(port, () => {
+    console.log("listening on " + port);
+});
